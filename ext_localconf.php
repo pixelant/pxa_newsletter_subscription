9085f3bb39ca7394df9dc3402425d2f57b3638ca
Andriy Oprysko <pyvasyk@gmail.com>
2019-08-20 16:19:23 +0300

    [TASK] use closure in ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,16 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Pixelant.' . $_EXTKEY,
-    'Subscription',
-    [
-        'NewsletterSubscription' => 'form,ajax,confirm',
-    ],
-    // non-cacheable actions
-    [
-        'NewsletterSubscription' => 'ajax,confirm'
-    ]
-);
+(function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Pixelant.pxa_newsletter_subscription',
+        'Subscription',
+        [
+            'NewsletterSubscription' => 'form,ajax,confirm',
+        ],
+        // non-cacheable actions
+        [
+            'NewsletterSubscription' => 'ajax,confirm'
+        ]
+    );
+})();
