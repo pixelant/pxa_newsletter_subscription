37177bfbc260287cbb2e2d9dd40ad53f62a14534
Andriy Oprysko <pyvasyk@gmail.com>
2019-08-22 13:20:48 +0300

    [WIP] working in JS

--- Resources/Public/Js/Form.js
+++ Resources/Public/Js/Form.js
@@ -1,56 +1,99 @@
-(function (w, $) {
-	var submitElement = $('[data-identifier="submit"]', 'form[data-form="pxa-newsletter-subscription-form"]');
+"use strict";
 
-	submitElement.on('click', function (e) {
-		e.preventDefault();
+const PxaNewsLetterSubscription = function (formSelector, callbackOnAjaxResponse) {
+	this.formSelector = formSelector;
+	this.callbackOnAjaxResponse = callbackOnAjaxResponse || false;
+};
 
-		var currentButton = $(this),
-			formElement = currentButton.parents('form[data-form="pxa-newsletter-subscription-form"]'),
-			spinnerElement = $('[data-identifier="spinner"]', formElement),
-			data = formElement.serializeArray();
+PxaNewsLetterSubscription.prototype = {
+	/**
+	 * Form DOM
+	 */
+	form: null,
 
-		data.push({name: currentButton.attr('name'), value: 1});
+	/**
+	 * Init everything
+	 */
+	init: function () {
+		let self = this;
+		this.form = document.querySelector(this.formSelector);
 
-		// Disable button, and fade in spinner
-		spinnerElement.fadeIn(50);
-		currentButton.prop('disabled', true);
+		// If forms were found
+		if (this.form.length > 0) {
+			this.form.addEventListener('submit', function (e) {
+				/*e.preventDefault();
 
-		$.ajax({
-			//type of receiving data
-			type: 'POST',
-			url: formElement.attr('action'),
-			data: data,
-			dataType: 'JSON'
-		}).done(function (response) {
-			//ajax sends msg from php, which informs user, what has happens
-			var message = $('<div/>', {
-				class: 'alert js__ajax-response',
-				text: response.message
-			});
-			formElement.after(message);
+				self._ajaxRequest(this);*/
+			}, true);
+		}
+	},
 
-			if (response.success) {
-				// display message
-				message.addClass('alert-success');
-				// hide form
-				formElement.hide();
-			} else {
-				// display message and set message to disapear after 5 sec.
-				message.addClass('alert-danger').delay(5000).fadeOut('slow');
+	/**
+	 * Send form with ajax
+	 *
+	 * @param form
+	 * @private
+	 */
+	_ajaxRequest: function (form) {
+		let formData = new FormData(form),
+			url = form.action;
+
+		let xmlHttpRequest = new XMLHttpRequest();
+
+		// Define what happens on successful data submission
+		xmlHttpRequest.addEventListener('load', (response) => {
+			if (!this._isFunction(this.callbackOnAjaxResponse)) {
+				console.log(response);
+
+				return;
 			}
-		}).fail(function (jqXHR, textStatus) {
-			// Set message and set it to disapear after 5 sec.
-			var message = $('<div/>', {
-				class: 'alert alert-danger js__ajax-response',
-				text: textStatus
-			});
-			formElement.after(message);
-			message.delay(5000).fadeOut('slow');
-		}).always(function () {
-			// Hide spinner and enable inputs again
-			spinnerElement.fadeOut(50);
-			currentButton.prop('disabled', false);
-		}); //end ajax
-	}); //end submit
 
-})(window, jQuery);
+			// Custom function
+			this._customResponseProcessing(response, form);
+		});
+
+		// Define what happens in case of error
+		xmlHttpRequest.addEventListener('error', (response) => {
+			if (!this._isFunction(this.callbackOnAjaxResponse)) {
+				console.log(response);
+
+				return;
+			}
+
+			// Custom function
+			this._customResponseProcessing(response, form);
+		});
+
+		// Set up our request
+		xmlHttpRequest.open('POST', url);
+
+		// Send our FormData object; HTTP headers are set automatically
+		xmlHttpRequest.send(formData);
+	},
+
+	/**
+	 * Process response with given callback
+	 *
+	 * @param response
+	 * @param form
+	 * @private
+	 */
+	_customResponseProcessing: function(response, form) {
+		this.callbackOnAjaxResponse({
+			response: response,
+			form: form
+		});
+	},
+
+	/**
+	 *
+	 * @param func
+	 * @returns {boolean}
+	 * @private
+	 */
+	_isFunction: function (func) {
+		return typeof func === 'function';
+	}
+};
+
+(new PxaNewsLetterSubscription('[data-form="pxa-newsletter-subscription-form"]')).init();
