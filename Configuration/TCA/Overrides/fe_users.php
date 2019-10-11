f8a21fd5f5c8375735eec5f50ecbc476eea05ff4
Andriy Oprysko <pyvasyk@gmail.com>
2019-10-11 10:50:25 +0300

    [BUGFIX] add email to list if label_alt is set

--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -3,7 +3,9 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 (function () {
     // Use email as alternative label
-    $labelAlt = $GLOBALS['TCA']['fe_users']['ctrl']['label_alt'] ? 'email' : 'email';
+    $labelAlt = $GLOBALS['TCA']['fe_users']['ctrl']['label_alt']
+        ? $GLOBALS['TCA']['fe_users']['ctrl']['label_alt'] . ',email'
+        : 'email';
 
     $GLOBALS['TCA']['fe_users']['ctrl']['label_alt'] = $labelAlt;
 })();
