43ac829da6f78341460a8e5060a5b174483940d1
Mathias Bolt Lesniak <mathias@pixelant.no>
2020-09-11 10:55:29 +0200

    [TASK] Set PHP requirement to include 7.4 (#38)
    * [TASK] Set PHP requirement to ^7.2 in composer.json
    
    * [TASK] Set PHP requirement to 7.2.0-7.4.99 in ext_emconf.php
    
    * [TASK] Set version to 9.0.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,11 +27,11 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '9.0.4',
+    'version' => '9.0.5',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.9.99',
-            'php' => '7.2.0-7.3.999'
+            'php' => '7.2.0-7.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
