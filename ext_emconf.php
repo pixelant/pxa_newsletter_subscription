2604264817cf7974ef7fc74d0312ff12d32cff42
Mathias Bolt Lesniak <mathias@pixelant.no>
2020-09-11 10:34:49 +0200

    [TASK] Set PHP requirement to 7.2.0-7.4.99 in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.9.99',
-            'php' => '7.2.0-7.3.999'
+            'php' => '7.2.0-7.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
